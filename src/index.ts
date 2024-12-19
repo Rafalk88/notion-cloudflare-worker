645cfdd95bf24a1d57cd9ed6e8d496f1d9128c2a
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2024-12-19 17:35:36 +0100

    feat: basic configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+import OpenAI from "openai";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+type Bindings = {
+	OPEN_AI_KEY: string;
+	AI: Ai;
+}
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(
+	'/*',
+	cors({
+		origin: '*', // Allow request from nextjs
+		allowHeaders: [
+			'X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-Type'
+		], // Add Content-Type to the allowed headers to fix CORS
+		allowMethods: ['POST', 'GET', 'OPTIONS', 'PUT'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true
+	})
+);
+
